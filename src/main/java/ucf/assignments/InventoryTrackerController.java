789f3b24cc3a6209235b66916b32a1f9e59c86f2
C.Gagne <crystal.r.gagne@gmail.com>
2021-07-21 12:42:48 -0400

    Added almost all core functions.

--- src/main/java/ucf/assignments/InventoryTrackerController.java
+++ src/main/java/ucf/assignments/InventoryTrackerController.java
@@ -5,8 +5,11 @@
 
 package ucf.assignments;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.TableView;
 
 import java.math.BigDecimal;
@@ -17,4 +20,43 @@ public class InventoryTrackerController {
     @FXML private TableColumn<Item, BigDecimal> valueColumn;
     @FXML private TableColumn<Item, String> serialNumberColumn;
     @FXML private TableColumn<Item, String> itemNameColumn;
+
+    @FXML private void initialize()
+    {
+        tableView.setEditable(true);
+
+    }
+
+    private void addItemClicked(ActionEvent clickedAddItem)
+    {
+
+    }
+    public void removeItemClicked(ActionEvent clickedDeleteItem)
+    {
+
+    }
+
+
+    private void editItemValue(CellEditEvent cellToEdit)
+    {
+
+    }
+    private void editSerialNumber(CellEditEvent cellToEdit)
+    {
+
+    }
+    private void editName(CellEditEvent cellToEdit)
+    {
+
+    }
+
+
+    private void saveItemClicked(EventHandler clickedSaveItem)
+    {
+
+    }
+    private void loadItemClicked(EventHandler clickedLoadItem)
+    {
+
+    }
 }
